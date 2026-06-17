0227a671214966f8937f92d0b1505a31b2125fb3
Soares <mattheussbarbosa@hotmail.com>
2026-06-17 13:55:34 -0300

    creating config file when stalling the mongo

--- InstallCeltaBSPDV/InstallMongoDb.bat
+++ InstallCeltaBSPDV/InstallMongoDb.bat
@@ -1,2 +1,27 @@
-msiexec.exe /l*v mdbinstall.log  /qb /i C:\Install\PDV\Database\mongodb.msi ^
-            SHOULD_INSTALL_COMPASS="0"
+@echo off
+:: 1. Run the silent MSI installation
+echo Installing MongoDB 7.0...
+msiexec.exe /l*v mdbinstall.log /qb /i C:\Install\PDV\Database\mongodb.msi SHOULD_INSTALL_COMPASS="0" ADDLOCAL="ServerService"
+
+:: 2. Wait for installation to finish and create the remote configuration file
+echo Writing remote configuration rules...
+set "CFG_FILE=C:\Program Files\MongoDB\Server\7.0\bin\mongod.cfg"
+
+(
+echo storage:
+echo   dbPath: C:\Program Files\MongoDB\Server\7.0\data
+echo systemLog:
+echo   destination: file
+echo   logAppend: true
+echo   path: C:\Temp\mongod.log
+echo net:
+echo   port: 27017
+echo   bindIp: 127.0.0.1,Your-Server-Public-IP
+) > "%CFG_FILE%"
+
+:: 3. Restart the Windows service to apply the remote ip rules
+echo Restarting MongoDB Service...
+net stop MongoDB
+net start MongoDB
+
+echo Done! MongoDB is now configured for remote connections.
